3fe0dfb1b4af368434edf29ef7b794d189b7c32b
Melissa <melissapotter.student@careerdevs.com>
2018-12-18 23:11:00 +0000

    .innerHTML, querySelector, += adding HTML

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name').innerText;
+var books = document.querySelectorAll('#book-list li .name');
 
-// console.log(wmf); 
+Array.from(books).forEach(function (book) {
+   console.log (book.textContent += ' (book title)');
+});
 
-var books = document.querySelector('#book-list li .name').innerText;
-
-// console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-
-// console.log(books);
-
-Array.from(books).forEach(function(book){
-    console.log(book);
-})
+const bookList =document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
