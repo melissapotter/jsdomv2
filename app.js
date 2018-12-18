fb19aeb2a1e5adfde9a88052bea81f63557b9f8d
Melissa <melissapotter.student@careerdevs.com>
2018-12-18 01:39:22 +0000

    nth child .name, querySelectorAll

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var titles = document.getElementsByClassName('title');
+const wmf = document.querySelector('#book-list li:nth-child(2) .name').innerText;
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
+// console.log(wmf); 
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-});
+var books = document.querySelector('#book-list li .name').innerText;
 
+// console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+
+// console.log(books);
+
+Array.from(books).forEach(function(book){
+    console.log(book);
+})
