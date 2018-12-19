6fb109aaecb2f520ccb5c62884f6a1d006edb007
Melissa <melissapotter.student@careerdevs.com>
2018-12-19 00:50:14 +0000

    .forms, addForm, input[type=text].value

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
- const list = document.querySelector('#book-list ul');
- 
- //delete books
- list.addEventListener('click', function(e){
-     if(e.target.className == 'delete'){
-     const li =e.target.parentElement; 
-     list.removeChild(li);
-     }
- });
+const list = document.querySelector('#book-list ul');
 
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
 
+// add book-list
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault(); 
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+});
